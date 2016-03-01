8e55f53697ef5ed24668240cc87da6b70e932084
Joshua Ginn <joshua.ginn94@gmail.com>
2016-03-01 17:03:28 +0000

    Clean Up
    For ref #9, clean up extra duplicated line from quiz route, and
    simplify the video route.

--- app/routes/video.rb
+++ app/routes/video.rb
@@ -12,25 +12,9 @@ class Flippd < Sinatra::Application
             @phase = phase
             @video = video
             @comments = Comment.all(:videoId => @video["id"])
-          end
-        end
-      end
-    end
-
-    @phases.each do |phase|
-      phase['topics'].each do |topic|
-        topic['videos'].each do |video|
-          if video["id"] == params['id'].to_i + 1
+          elsif video["id"] == params['id'].to_i + 1
             @next_video = video
-          end
-        end
-      end
-    end
-
-    @phases.each do |phase|
-      phase['topics'].each do |topic|
-        topic['videos'].each do |video|
-          if video["id"] == params['id'].to_i - 1
+          elsif video["id"] == params['id'].to_i - 1
             @previous_video = video
           end
         end
