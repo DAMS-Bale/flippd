daa2ff2b8ea4dcfc39b6ca546a76c8fd922c210a
Joshua Ginn <joshua.ginn94@gmail.com>
2016-03-01 15:25:57 +0000

    Test Points Awarded For Quizzes
    Add tests that verify that points are awarded for completing
    quizzes, and stored on the dashboard. For ref #9.

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -72,17 +72,44 @@ feature "a user dashboard page" do
           "#{@quiz.name} 0.0% 0 / 1 #{DateTime.now.strftime("%d/%m/%Y")}")
       end
 
+      it "should have awarded no points for the quiz" do
+        expect(page).to have_content("0 Points")
+      end
+
       context "after getting 100%" do
         before(:each) do
           visit('/quiz/' + @quiz.id.to_s)
           choose @answer1_1.id
+          @time100 = DateTime.now
           click_on "Mark"
           visit_dashboard
         end
 
         it "should have the best result listed" do
           expect(page).to have_content(
-            "#{@quiz.name} 100.0% 1 / 1 #{DateTime.now.strftime("%d/%m/%Y")}")
+            "#{@quiz.name} 100.0% 1 / 1 #{@time100.strftime("%d/%m/%Y  %H:%M")}")
+        end
+
+        it "should have awarded a point for the correct answer" do
+          expect(page).to have_content("1 Points")
+        end
+
+        context "after getting 0% again" do
+          before(:each) do
+            visit('/quiz/' + @quiz.id.to_s)
+            choose @answer1_2.id
+            click_on "Mark"
+            visit_dashboard
+          end
+
+          it "should continue listing the best result" do
+            expect(page).to have_content(
+              "#{@quiz.name} 100.0% 1 / 1 #{@time100.strftime("%d/%m/%Y  %H:%M")}")
+          end
+
+          it "should have still awarded a point for the correct answer" do
+            expect(page).to have_content("1 Points")
+          end
         end
       end
     end
